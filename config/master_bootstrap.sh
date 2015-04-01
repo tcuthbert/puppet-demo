59c614ded1a8439826613ab2eb9980815ced040c
Thomas Cuthbert <tcuthbert90@gmail.com>
2015-04-01 21:44:32 +0800

    Updated bootstrapping scripts

--- config/master_bootstrap.sh
+++ config/master_bootstrap.sh
@@ -3,10 +3,14 @@
 #source /root/.bashrc
 #puppet module install zack-r10k 
 #/opt/puppet/bin/gem install r10k
-cp /vagrant/config/{hiera.yaml,r10k.yaml} /etc/puppetlabs/puppet/
-cd /etc/
-ln -fs /etc/puppetlabs/puppet/{hiera.yaml,r10k.yaml}
-puppet apply -e 'class { "r10k": remote => "https://github.com/tcuthbert/environmentsrepo"  }'
+cp /vagrant/config/hiera.yaml /etc/puppetlabs/puppet/
+ln -fs /etc/puppetlabs/puppet/hiera.yaml /etc/hiera.yaml
+mkdir -p /etc/puppetlabs/puppet/environments/production
+puppet module install zack-r10k && \
+puppet apply -e 'class { "r10k": remote => "https://github.com/tcuthbert/environmentsrepo" }' && \
+rm -fr /etc/puppetlabs/puppet/environments/production/*
+r10k deploy environment -pv 
+#puppet apply -e 'class { "r10k": remote => "https://github.com/tcuthbert/environmentsrepo"  }'
 #puppet apply -e "/vagrant/environments/site/base/manifests/profiles/r10k_installation.pp"
 #cp /var/lib/peadmin/.mcollective ~/
 #mco r10k synchronize
